1d10fd0ed76018117ce826d49d6ebe2b4264dac9
Luong Vu <joturako@gmail.com>
2021-12-15 01:55:54 +0700

    disable branding output

--- components/PapercupsBranding.tsx
+++ components/PapercupsBranding.tsx
@@ -1,24 +1,6 @@
-import React from 'react';
-import {Flex, Link} from 'theme-ui';
 
 const PapercupsBranding = () => {
-  return (
-    <Flex m={2} sx={{justifyContent: 'center', alignItems: 'center'}}>
-      <Link
-        href="https://papercups.io?utm_source=papercups&utm_medium=chat&utm_campaign=chat-widget-link"
-        target="_blank"
-        rel="noopener noreferrer"
-        sx={{
-          color: 'gray',
-          opacity: 0.8,
-          transition: '0.2s',
-          '&:hover': {opacity: 1},
-        }}
-      >
-        Powered by Papercups
-      </Link>
-    </Flex>
-  );
+  return null; 
 };
 
 export default PapercupsBranding;
